9f774c1086f3370227923ced47a73a9447c7479e
Malek Jendoubi <malek.jendoubi@uni-rostock.de>
2024-01-31 09:27:35 +0100

    added logging like example

--- i2c_bmp581/src/main.c
+++ i2c_bmp581/src/main.c
@@ -14,50 +14,71 @@
 #include "bmp5.h"
 #include "common.h"
 
+/*!
+ *  @brief This internal API is used to set configurations of the sensor.
+ *
+ *  @param[in,out] osr_odr_press_cfg : Structure instance of bmp5_osr_odr_press_config
+ *  @param[in] dev                   : Structure instance of bmp5_dev.
+ *
+ *  @return Status of execution.
+ */
+static int8_t set_config(struct bmp5_osr_odr_press_config *osr_odr_press_cfg, struct bmp5_dev *dev);
+
+
+/*!
+ *  @brief This internal API is used to get sensor data.
+ *
+ *  @param[in] osr_odr_press_cfg : Structure instance of bmp5_osr_odr_press_config
+ *  @param[in] dev               : Structure instance of bmp5_dev.
+ *
+ *  @return Status of execution.
+ */
 static int8_t get_sensor_data(const struct bmp5_osr_odr_press_config *osr_odr_press_cfg, struct bmp5_dev *dev);
+
+/*!
+ *  @brief Prints the execution status of the APIs.
+ *
+ *  @param[in] api_name : Name of the API whose execution status has to be printed.
+ *  @param[in] rslt     : Error code returned by the API whose execution status has to be printed.
+ *
+ *  @return void.
+ */
+void bmp5_error_codes_print_result(const char api_name[], int8_t rslt);
 
 /* Register the logger */
 LOG_MODULE_REGISTER(BMPI2C, LOG_LEVEL_DBG);
 
 int main(void)
 {
-    volatile int8_t rslt;
+    int8_t rslt;
     struct bmp5_dev dev;
-    struct bmp5_osr_odr_press_config osr_odr_press_cfg = {0};
-
-    LOG_INF("main\n");
+    struct bmp5_osr_odr_press_config osr_odr_press_cfg = { 0 };
 
     /* Interface reference is given as a parameter
      * For I2C : BMP5_I2C_INTF
      * For SPI : BMP5_SPI_INTF
      */
     rslt = bmp5_interface_init(&dev, BMP5_I2C_INTF);
+    bmp5_error_codes_print_result("bmp5_interface_init", rslt);
 
     if (rslt == BMP5_OK)
     {
-        LOG_INF("bmp5_interface_init\n");
-        
         rslt = bmp5_init(&dev);
+        bmp5_error_codes_print_result("bmp5_init", rslt);
+
         if (rslt == BMP5_OK)
         {
-            LOG_INF("bmp5_init\n");
+            rslt = set_config(&osr_odr_press_cfg, &dev);
+            bmp5_error_codes_print_result("set_config", rslt);
+        }
+
+        if (rslt == BMP5_OK)
+        {
             rslt = get_sensor_data(&osr_odr_press_cfg, &dev);
-            LOG_INF("get_sensor_data\n");
+            bmp5_error_codes_print_result("get_sensor_data", rslt);
         }
     }
-    LOG_INF("before loop\n");
-
-    int loop_count;
-    while (1)
-    {
-        LOG_INF("nRF Connect SDK Fundamentals: %d\n", loop_count);
-        rslt = get_sensor_data(&osr_odr_press_cfg, &dev);
-        LOG_INF("status: %d\n", rslt);
-        loop_count++;
-        k_msleep(2*1000); 
-    }
-
-    return 0;
+    return rslt;
 }
 
 static int8_t get_sensor_data(const struct bmp5_osr_odr_press_config *osr_odr_press_cfg, struct bmp5_dev *dev)
@@ -90,4 +111,112 @@ static int8_t get_sensor_data(const struct bmp5_osr_odr_press_config *osr_odr_pr
     }
 
     return rslt;
+}
+
+static int8_t set_config(struct bmp5_osr_odr_press_config *osr_odr_press_cfg, struct bmp5_dev *dev)
+{
+    int8_t rslt;
+    struct bmp5_iir_config set_iir_cfg;
+    struct bmp5_int_source_select int_source_select;
+
+    rslt = bmp5_set_power_mode(BMP5_POWERMODE_STANDBY, dev);
+    bmp5_error_codes_print_result("bmp5_set_power_mode1", rslt);
+
+    if (rslt == BMP5_OK)
+    {
+        /* Get default odr */
+        rslt = bmp5_get_osr_odr_press_config(osr_odr_press_cfg, dev);
+        bmp5_error_codes_print_result("bmp5_get_osr_odr_press_config", rslt);
+
+        if (rslt == BMP5_OK)
+        {
+            /* Enable pressure */
+            osr_odr_press_cfg->press_en = BMP5_ENABLE;
+
+            rslt = bmp5_set_osr_odr_press_config(osr_odr_press_cfg, dev);
+            bmp5_error_codes_print_result("bmp5_set_osr_odr_press_config", rslt);
+        }
+
+        if (rslt == BMP5_OK)
+        {
+            set_iir_cfg.set_iir_t = BMP5_IIR_FILTER_COEFF_1;
+            set_iir_cfg.set_iir_p = BMP5_IIR_FILTER_COEFF_1;
+            set_iir_cfg.shdw_set_iir_t = BMP5_ENABLE;
+            set_iir_cfg.shdw_set_iir_p = BMP5_ENABLE;
+
+            rslt = bmp5_set_iir_config(&set_iir_cfg, dev);
+            bmp5_error_codes_print_result("bmp5_set_iir_config", rslt);
+        }
+
+        if (rslt == BMP5_OK)
+        {
+            rslt = bmp5_configure_interrupt(BMP5_PULSED, BMP5_ACTIVE_HIGH, BMP5_INTR_PUSH_PULL, BMP5_INTR_ENABLE, dev);
+
+            bmp5_error_codes_print_result("bmp5_configure_interrupt", rslt);
+
+            if (rslt == BMP5_OK)
+            {
+                /* Note : Select INT_SOURCE after configuring interrupt */
+                int_source_select.drdy_en = BMP5_ENABLE;
+                rslt = bmp5_int_source_select(&int_source_select, dev);
+                bmp5_error_codes_print_result("bmp5_int_source_select", rslt);
+            }
+        }
+
+        /* Set powermode as continous */
+        rslt = bmp5_set_power_mode(BMP5_POWERMODE_CONTINOUS, dev);
+        bmp5_error_codes_print_result("bmp5_set_power_mode", rslt);
+    }
+
+    return rslt;
+}
+
+/*!
+ *  @brief Prints the execution status of the APIs.
+ */
+void bmp5_error_codes_print_result(const char api_name[], int8_t rslt)
+{
+    if (rslt != BMP5_OK)
+    {
+        LOG_INF("%s\t", api_name);
+        if (rslt == BMP5_E_NULL_PTR)
+        {
+            LOG_INF("Error [%d] : Null pointer\r\n", rslt);
+        }
+        else if (rslt == BMP5_E_COM_FAIL)
+        {
+            LOG_INF("Error [%d] : Communication failure\r\n", rslt);
+        }
+        else if (rslt == BMP5_E_DEV_NOT_FOUND)
+        {
+            LOG_INF("Error [%d] : Device not found\r\n", rslt);
+        }
+        else if (rslt == BMP5_E_INVALID_CHIP_ID)
+        {
+            LOG_INF("Error [%d] : Invalid chip id\r\n", rslt);
+        }
+        else if (rslt == BMP5_E_POWER_UP)
+        {
+            LOG_INF("Error [%d] : Power up error\r\n", rslt);
+        }
+        else if (rslt == BMP5_E_POR_SOFTRESET)
+        {
+            LOG_INF("Error [%d] : Power-on reset/softreset failure\r\n", rslt);
+        }
+        else if (rslt == BMP5_E_INVALID_POWERMODE)
+        {
+            LOG_INF("Error [%d] : Invalid powermode\r\n", rslt);
+        }
+        else
+        {
+            /* For more error codes refer "*_defs.h" */
+            LOG_INF("Error [%d] : Unknown error code\r\n", rslt);
+        }
+    }        else
+        {
+            /* For more error codes refer "*_defs.h" */
+            LOG_INF("%s\t", api_name);
+            LOG_INF("[%d] : BMP5 OK\r\n", rslt);
+        }
+
 }
