2cdef6f79e3b1e7533e5fbed6c4c5cbc73ef1c13
Malek Jendoubi <malek.jendoubi@uni-rostock.de>
2024-02-06 13:09:46 +0100

    Modified readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# projektmsc-jendoubi-wise2223
-Projekt Msc. Elektrotechnik Malek Jendoubi - Wintersemester 2022/2023
+# Projekt Msc. Elektrotechnik Malek Jendoubi - Wintersemester 2022/2023
+---
+Projekt besteht aus zwei teile:
+
+* Ein BLE-Server (nRF52832) empfängt Luftdruckwerte von einem BMP581-Sensor von Bosch Sensortec über eine I2C-Verbindung. Er gibt sie über einen BLE-GATT-Server aus. 
+
+* Ein Python-Client auf einem Windows-PC stellt eine Verbindung zum NFR her und empfängt die Luftdruck- und Timestampwerte.
+
---
+++ nrf_52_server/build/image_preload.cmake
@@ -0,0 +1,9 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52_adafruit_feather" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/nordic/myapps/projektmsc-jendoubi-wise2223/i2c_bmp581/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_OPTIMIZATIONS "y" CACHE INTERNAL "NCS child image controlled")
+set(CLI_CONFIG_DEBUG_THREAD_INFO "y" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "c:/nordic/myapps/projektmsc-jendoubi-wise2223/i2c_bmp581/nrf52_adafruit_feather.overlay" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
